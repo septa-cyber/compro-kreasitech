57ca2ef7bc2ae6ac946a675d1254cd49019d2fc0
Rian Rama Nugraha <ryan23nugraha23@gmail.com>
2026-01-20 14:15:48 +0700

    feat: add allowed development origin for production environment in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['localhost', '127.0.0.1', '192.168.1.152'],
-};
+  allowedDevOrigins: ['localhost', '127.0.0.1', '192.168.1.152', 'https://compro-kreasitech.kreasi.dev'],
+}
 
 export default nextConfig;
