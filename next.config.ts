4796ba59af0284642c7dcf6ba604f6f5a19f64ad
Rian Rama Nugraha <ryan23nugraha23@gmail.com>
2026-01-20 14:14:49 +0700

    feat: update Next.js configuration to specify allowed development origins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['localhost', '127.0.0.1', '192.168.1.152'],
 };
 
 export default nextConfig;
