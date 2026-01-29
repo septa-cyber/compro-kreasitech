e14b339bd1289b352e907a48ee3fdd16e69718e7
Septikiwir <wahyuseptapramudya@students.amikom.ac.id>
2026-01-29 15:55:36 +0700

    feat: Update Hero component with new partner logos and enhance marquee animation speed

--- app/components/landing/Hero.tsx
+++ app/components/landing/Hero.tsx
@@ -28,7 +28,60 @@ export default function Hero() {
         }
     ];
 
-    const partnerLogos = Array(7).fill("/assets/images/Logo.svg");
+    const partnerLogos = [
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/4life.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/AWS.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Ayro_Trading_Jaya_1.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Bekawan.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/BIG.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/BP.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/btrade.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/callisto erp.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Ceicilia.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/CKS_PEARL_LOGO_MASTER.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Danamon.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/danarhadi.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/eduline.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/FIF Group.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/gracia bag.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/IKN.jpeg",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/kalbu.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/kusuma medika.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo bayarind 1.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo bisa basi.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo callisto.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo eds.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo GSK.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo khs.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo medlink.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo octa meyer.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo spfio.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Logo-Aizonee-v3.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo-bsi.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Logo-QuBisa.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/LOGO-SINODE-1.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo-zep.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/logo_central_small.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/mkw.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Nyalacinta.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/obviously sustainable.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/ohana.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Outsystems.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/PPU.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/PT Ako Media Asia.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/queeri.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Sangfor.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/save-the-children-logo-horizontal-color-positive_2.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Sehat_meyer.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Sequis Tower.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/spfio.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/susi.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/This is april.jpg",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/Titik_nol.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/tokocrypto.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/UT - Forum Study Ilmu Hukum.png",
+        "/assets/images/LOGO CLIENT/LOGO CLIENT/xendit.png",
+    ];
 
     return (
         <section className="relative pt-40 pb-20 lg:pt-54 lg:pb-16 overflow-hidden bg-[#F4F4F7]">
@@ -161,13 +214,13 @@ export default function Hero() {
                     onMouseLeave={() => setIsTrustedByHovered(false)}
                 >
                     <div
-                        className="flex space-x-12 shrink-0 animate-marquee items-center"
+                        className="flex space-x-12 shrink-0 animate-marquee-slow items-center"
                         style={{ animationPlayState: isTrustedByHovered ? 'paused' : 'running' }}
                     >
                         {/* Duplicate content for marquee effect */}
                         {[...partnerLogos, ...partnerLogos, ...partnerLogos, ...partnerLogos].map((logo, index) => (
-                            <div key={index} className="flex items-center gap-2 opacity-60 grayscale hover:grayscale-0 transition duration-500">
-                                <Image src={logo} alt="Kreasitech" width={100} height={32} className="h-8 w-auto" />
+                            <div key={index} className="flex items-center gap-2 grayscale hover:grayscale-0 opacity-70 hover:opacity-100 transition duration-500">
+                                <Image src={logo} alt="Partner Logo" width={120} height={48} className="h-10 w-auto object-contain" />
                             </div>
                         ))}
                     </div>
